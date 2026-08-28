4ee7b979be70981f9a9e93ec09a037cc7e7d5c37
Reynerio Samos <2018119235@ub.edu.bz>
2026-08-28 14:13:00 -0600

    Delete gatekeeper-asynchronous- code directory
    deleted duplicate folder

--- gatekeeper-asynchronous- code/migrations/000001_create_init_extensions_and_types.up.sql
+++
@@ -1,9 +0,0 @@
--- Filename: 000001_create_init_extensions_and_types.up.sql
-
-BEGIN;
-
-CREATE TYPE consumer_status AS ENUM ('active', 'suspended', 'terminated');
-CREATE TYPE key_status     AS ENUM ('active', 'rotating', 'revoked');
-CREATE TYPE job_status     AS ENUM ('queued', 'processing', 'completed', 'failed', 'cancelled');
-
-COMMIT;
